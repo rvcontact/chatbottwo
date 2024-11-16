235edd4ce9f21c20909087b5887007683ea76429
rvcontact <contact@richardvose.com>
2024-11-16 17:25:12 -0500

    updated endpoint location

--- vertex-ai.js
+++ vertex-ai.js
@@ -14,7 +14,7 @@ const client = new PredictionServiceClient();
 
 // In vertex-ai.js
 const projectId = process.env.PROJECT_ID;
-const location = 'us-central1';
+const location = 'us-east4';
 const endpointId = process.env.ENDPOINT_ID;
 const endpointName = `projects/${projectId}/locations/${location}/endpoints/${endpointId}`;
 
