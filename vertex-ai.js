22d5838a69eb441a7fc032b5c93ba3a859c22e55
rvcontact <contact@richardvose.com>
2024-11-16 20:37:05 -0500

    updated vertex-ai.js

--- vertex-ai.js
+++ vertex-ai.js
@@ -4,13 +4,6 @@ const { PredictionServiceClient } = require('@google-cloud/aiplatform');
 // Create a new client instance
 const client = new PredictionServiceClient();
 
-// Retrieve environment variables
-//OLD const projectId = process.env.PROJECT_ID;
-//OLD const location = 'us-central1';
-//OLD const endpointId = process.env.ENDPOINT_ID;
-
-// Manually construct the endpoint name
-//OLD const endpointName = `projects/${projectId}/locations/${location}/endpoints/${endpointId}`;
 
 // In vertex-ai.js
 const projectId = process.env.PROJECT_ID;
